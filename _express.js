04dc90b1c0f5635f459be582188fdf954dc76dc8
Atsushi <atsushithesushi@gmail.com>
2021-06-13 13:19:40 -0300

    V2 Simpl.DB + Express.JS

--- _express.js
+++ _express.js
@@ -11,7 +11,7 @@ server.use(express.urlencoded({
 
 server.use("/login/api", postRoute);
 
-server.get("/", async(req, res) => {
+server.get("/", (req, res) => {
     res.sendFile(__dirname + '/views/login.html');
 });
 
